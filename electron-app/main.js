f8d57378353acb1138da65d9ccf7f2a0fbd897d8
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2025-06-03 06:09:13 -0700

    added an electron app template that runs using the web app

---
+++ electron-app/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('http://172.18.112.1:3000/'); // Example site
+});
